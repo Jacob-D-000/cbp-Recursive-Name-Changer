e1fbc5d9f9143695262953d98695ddca5345e0b1
Jacob-D-000 <w0478482@nscc.ca>
2023-01-26 11:09:21 -0400

    Let the games begin

---
+++ cbpRecursive.ps1
@@ -0,0 +1,7 @@
+# Title: My Dev Setup
+# Author: Jacob Dimoff
+# Date started: 26-1-23 
+# This Recursive File will be used to rename the codeblocks Project files with the cbp suffix.
+
+
+
---
+++ ps1RW-/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("this is a test lolololollololololololololololo\n");
+    return 0;
+}
