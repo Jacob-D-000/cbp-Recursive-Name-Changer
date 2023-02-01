7780233ef700de3b45ba70d413c7424443cb9368
Jacob-D-000 <W0478482@nscc.ca>
2023-01-31 21:40:15 -0400

    Done for the night

--- cbpRecursive.py
+++ cbpRecursive.py
@@ -11,4 +11,32 @@ import xml.parsers.expat
 
 # Find the proper file location
 
-os.chdir
+
+def cdpwd():
+    # define a var with current work folder
+    curdir = os.path.dirname(__file__)
+    # define the control variable for the try expect
+    convar = 0
+    # try to get he user to insert a path if it exists
+    while convar == 0:
+        try:
+            print(f"The current working directory is {curdir}. What is the relative path of the file you want to change(it must stat with a '\'?")
+            # ask for input
+            relpath = input()
+            # cat the pwd and relative path to form new working path
+            truepath = curdir + relpath
+            # change working path
+            os.chdir(truepath)
+            # convar acts as escape.
+            convar = 1
+        except:
+            print("This path doesn't exist please try again.")
+            convar = 0
+
+        
+
+def main():
+    cdpwd()
+    
+
+main()
